90ea6de1f944e64d107141f6f2c52fd00ef744f6
Robert Arellano <robert@bstelpaso.com>
2016-10-28 13:19:25 -0500

    Replace source from freeformatter.com to lipsum.com (fixes #2)
    Refactored code and changed source from freeformatter.com to lipsum.com. freeformatter.com changed configurations that no longer made it compatible to extract lorem ipsum text. Dropped the ability to generate sentences.

--- lipsum.rb
+++ lipsum.rb
@@ -1,33 +1,75 @@
 require 'open-uri'
 require 'net/http'
+require 'json'
 
-# Default parameters 
-how_many = 1
-mode = 'p'
+def set_params amount, what
+	# Set parameters for lipsum.com 
+	params = {
+		'amount' => "#{amount}",
+		'what' => "#{what}"
+	}
+end
 
-# Set Arguments if they exist
-how_many_arg = ARGV[0] if !ARGV[0].empty?
-how_many = how_many_arg.to_i if !how_many_arg.nil?
+def get_lipsum params
+	url = URI.parse('http://www.lipsum.com/feed/json')
+	resp = Net::HTTP.post_form(url, params)
+	content = resp.body
+end
 
-mode_arg = ARGV[1] if !ARGV[1].empty?
-mode = mode_arg.to_s if !mode_arg.nil?
+def add_extra_spacing content
+	# add extra spaces if string contains \n
+	content.gsub! '\n', '\n\n' if content.include? '\n'
+	content
+end
 
+def parse_content content
+	content = JSON.parse(content)
+	content["feed"]["lipsum"]
+end
 
-# Set parameters for freeformatter 
-params = {
-	'site' => 'lorem-ipsum', 
-	'mode' => "#{mode}", 
-	'howMany' => "#{how_many}",
-	'size' => 'SMALL', 
-	'includeHtml' => 'false' 
-}
+def remove_blah content
+	content = resp.body.gsub(/<\/\li>/, "\n").gsub(/<\/?[^>]*>/, "") if what == 'l'
+	content.strip
+end
 
-# Post and set content
-url = URI.parse('http://www.freeformatter.com/lorem-ipsum-generator.html')
-resp = Net::HTTP.post_form(url, params)
-content = resp.body
-content = resp.body.gsub(/<\/\li>/, "\n").gsub(/<\/?[^>]*>/, "") if mode == 'l'
-content = content.strip
+def prepare_content content
+	spaced_content = add_extra_spacing content
+	parse_content spaced_content
+end
 
-# Return content
-print content
+def return_content content
+	print content
+end
+
+def generate_lipsum amount, what
+	params = set_params amount, what
+	raw_content = get_lipsum params
+	content = prepare_content raw_content
+	return_content content
+end
+
+def set_amount
+	# Default arg
+	amount = 1
+	# Set Argument if they exist
+	amount_arg = ARGV[0] if !ARGV[0].empty?
+	amount = amount_arg.to_i if !amount_arg.nil?
+	return amount
+end
+
+def set_mode
+	# Default arg
+	what = 'paras'
+	# Set Argument if they exist
+	what_arg = ARGV[1] if !ARGV[1].empty?
+	what = what_arg.to_s if !what_arg.nil?
+	return what
+end
+
+def init
+	amount = set_amount
+	what = set_mode
+	generate_lipsum amount, what
+end
+
+init()
